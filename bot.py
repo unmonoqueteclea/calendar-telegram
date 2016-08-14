f907bfaae33bd662a3f263697b6ff3c4a7866589
Pablo Gonzalez Carrizo <pgonzalezcarrizo@gmail.com>
2016-08-14 15:56:41 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+import telebot
+from telebot import types
+import datetime
+from telegramcalendar import create_calendar
+
+bot = telebot.TeleBot("")
+current_shown_dates={}
+
+@bot.message_handler(commands=['calendar'])
+def get_calendar(message):
+    now = datetime.datetime.now() #Current date
+    chat_id = message.chat.id
+    date = (now.year,now.month)
+    current_shown_dates[chat_id] = date #Saving the current date in a dict
+    markup= create_calendar(now.year,now.month)
+    bot.send_message(message.chat.id, "Please, choose a date", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data[0:13] == 'calendar-day-')
+def get_day(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        day=call.data[13:]
+        date = datetime.datetime(int(saved_date[0]),int(saved_date[1]),int(day),0,0,0)
+        bot.send_message(chat_id, str(date))
+        bot.answer_callback_query(call.id, text="")
+
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'next-month')
+def next_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month+=1
+        if month>12:
+            month=1
+            year+=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'previous-month')
+def previous_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month-=1
+        if month<1:
+            month=12
+            year-=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Please, choose a date", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'ignore')
+def ignore(call):
+    bot.answer_callback_query(call.id, text="")
+
+
+bot.polling()
---
+++ telegramcalendar.py
@@ -0,0 +1,35 @@
+from telebot import types
+import calendar
+
+API_URL="http://127.0.0.1:5000/"
+TOKEN="244734389:AAFZNXWj7cvp5sb1gN-P0tQVYqd7SULnAbQ"
+
+def create_calendar(year,month):
+    markup = types.InlineKeyboardMarkup()
+    #First row - Month and Year
+    row=[]
+    row.append(types.InlineKeyboardButton(calendar.month_name[month]+" "+str(year),callback_data="ignore"))
+    markup.row(*row)
+    #Second row - Week Days
+    week_days=["M","T","W","R","F","S","U"]
+    row=[]
+    for day in week_days:
+        row.append(types.InlineKeyboardButton(day,callback_data="ignore"))
+    markup.row(*row)
+
+    my_calendar = calendar.monthcalendar(year, month)
+    for week in my_calendar:
+        row=[]
+        for day in week:
+            if(day==0):
+                row.append(types.InlineKeyboardButton(" ",callback_data="ignore"))
+            else:
+                row.append(types.InlineKeyboardButton(str(day),callback_data="calendar-day-"+str(day)))
+        markup.row(*row)
+    #Last row - Buttons
+    row=[]
+    row.append(types.InlineKeyboardButton("<",callback_data="previous-month"))
+    row.append(types.InlineKeyboardButton(" ",callback_data="ignore"))
+    row.append(types.InlineKeyboardButton(">",callback_data="next-month"))
+    markup.row(*row)
+    return markup
