20848f86cdd229ebd6f4d41737b8664b5333baa8
Alex Limontov <alex.limontov@gmail.com>
2020-03-22 16:16:52 +0300

    Poetry configuration for the project

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='calendar-telegram',
+      version='0.1.0',
+      description='Datepicker for python-telegram-bot',
+      author='',
+      author_email='',
+      url='https://github.com/chiselko6/calendar-telegram',
+      package_dir={'': 'src'},
+      packages=['calendar_telegram'],
+      install_requires=['python-telegram-bot>=12,<12.5'],
+     )
