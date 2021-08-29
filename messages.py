9c31c656729cc9b839d38283eeb5dc039ade4c6e
mhbahmani <mhbahmani79@gmail.com>
2021-08-29 13:32:58 +0430

    add some messages

--- messages.py
+++ messages.py
@@ -1,2 +1,10 @@
 CALENDAR_CALLBACK = "CALENDAR"
 JCALENDAR_CALLBACK = "JCALENDAR"
+
+start_message = "Hey {}! I am calender bot \n \n Please type /calendar or /jcalendar to view my power"
+
+calendar_message = "Please select a date: "
+jcalendar_message = "لطفا تاریخی را انتخاب کنید:"
+
+calendar_response_message = "You selected %s"
+jcalendar_response_message = "شما تاریخ %s را انتخاب کردید"
