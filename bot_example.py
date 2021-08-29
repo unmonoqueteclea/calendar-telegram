a4efff384dc6efb73f75846dbb9156054c5175fb
mhbahmani <mhbahmani79@gmail.com>
2021-08-29 13:32:58 +0430

    add some messages

--- bot_example.py
+++ bot_example.py
@@ -18,22 +18,25 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-def start(update,context):
-    context.bot.send_message(chat_id=update.message.chat_id,text="Hey {}! I am calender bot \n \n Please type /calendar to view my power".format(update.message.from_user.first_name),parse_mode=ParseMode.HTML)
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text=messages.start_message.format(update.message.from_user.first_name),
+        parse_mode=ParseMode.HTML)
 
 # A simple command to display the calender
-def calendar_handler(update,context):
-    update.message.reply_text(text="Please select a date: ",
+def calendar_handler(update, context):
+    update.message.reply_text(text=messages.calendar_message,
                     reply_markup=telegramcalendar.create_calendar())
     
 
 def jcalendar_handler(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
-        text="لطفا تاریخی را انتخاب کنید:",
+        text=messages.jcalendar_message,
         reply_markup=telegramjcalendar.create_calendar()
     )
 
-def inline_handler(update,context):
+def inline_handler(update, context):
     query = update.callback_query
     (kind, _, _, _, _) = utils.separate_callback_data(query.data)
     if kind == messages.CALENDAR_CALLBACK:
@@ -46,7 +49,7 @@ def inline_calendar_handler(update, context):
     selected,date = telegramcalendar.process_calendar_selection(update, context)
     if selected:
         context.bot.send_message(chat_id=update.callback_query.from_user.id,
-                        text="You selected %s" % (date.strftime("%d/%m/%Y")),
+                        text=messages.calendar_response_message % (date.strftime("%d/%m/%Y")),
                         reply_markup=ReplyKeyboardRemove())
 
 
@@ -54,22 +57,19 @@ def inline_jcalendar_handler(update: Update, context: CallbackContext):
     selected, date = telegramjcalendar.process_calendar_selection(context.bot, update)
     if selected:
         context.bot.send_message(chat_id=update.callback_query.from_user.id,
-                text="شما تاریخ %s را انتخاب کردید" % date,
+                text=messages.jcalendar_response_message % date,
                 reply_markup=ReplyKeyboardRemove())
 
 
-if TOKEN == "":
-    print("Please write TOKEN into file")
+if TOKEN == "": print("Please write TOKEN into file")
 else:
     updater = Updater(TOKEN,use_context=True)
     dp=updater.dispatcher
-
 
     dp.add_handler(CommandHandler("start",start))
     dp.add_handler(CommandHandler("calendar",calendar_handler))
     dp.add_handler(CommandHandler("jcalendar",jcalendar_handler))
     dp.add_handler(CallbackQueryHandler(inline_handler))
-    dp.add_handler(CallbackQueryHandler(inline_jcalendar_handler))
 
     updater.start_polling()
     updater.idle()
